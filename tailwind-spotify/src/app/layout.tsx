f9dff4053166460d8f9ac5d58f98047f62b9e6cc
Pedro Henrique <pedrohv20fernandes@gmail.com>
2023-04-18 01:17:14 -0300

    inicialização

---
+++ tailwind-spotify/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Spotify',
+  description: 'Spotify is a digital music service that gives you access to millions of songs.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='bg-zinc-900 text-zinc-50'>{children}</body>
+    </html>
+  )
+}
