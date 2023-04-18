9d0a6929a328942e77fda13fd03e311706e19e44
Pedro Henrique <pedrohv20fernandes@gmail.com>
2023-04-18 18:18:33 -0300

    v1 Spotify pronto

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -1,4 +1,7 @@
-import {Home as HomeIcon, Search, Library} from 'lucide-react'
+import { Footer } from '@/components/Footer'
+import { Sidebar } from '@/components/Sidebar'
+import { ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -7,22 +10,201 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-            <a href="">
-              Home
+        <Sidebar />
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-3">
+            <button className="rounded-full bg-black/40 p-1 ">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1 ">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
             </a>
-            <a href="">
-              Search
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
             </a>
-            <a href="">
-              Your Library
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
             </a>
-          </nav>
-        </aside>
-        <main className="flex-1 p-6">main</main>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong>Wasting Light</strong>
+              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">
+            Made for Pedro Henrique Fernandes
+          </h1>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Wallows, COIN, girl in red and more
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Wallows, COIN, girl in red and more
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Wallows, COIN, girl in red and more
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Wallows, COIN, girl in red and more
+              </span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/Album.jpg"
+                className="w-full"
+                width={120}
+                height={120}
+                alt="Capa do álbum Wasting Light"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">
+                Wallows, COIN, girl in red and more
+              </span>
+            </a>
+          </div>
+        </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+      <Footer />
     </div>
   )
 }
